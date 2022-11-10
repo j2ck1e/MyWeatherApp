65f259aab2baff0eb9954724f1c14156a06d2b3b
j2ck1e <j2ck1e86@gmail.com>
2022-11-10 22:51:51 +0300

    Added adapter & tested HoursFragment

--- app/src/main/java/com/practicum/myweatherapp/adapters/WeatherAdapter.kt
+++ app/src/main/java/com/practicum/myweatherapp/adapters/WeatherAdapter.kt
@@ -1,18 +1,43 @@
 package com.practicum.myweatherapp.adapters
 
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.ListAdapter
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.practicum.myweatherapp.R
 import com.practicum.myweatherapp.databinding.ListItemBinding
 
+class WeatherAdapter : ListAdapter<WeatherInfo, WeatherAdapter.Holder>(Comparator()) {
 
-class WeatherAdapter : ListAdapter<WeatherInfo, WeatherAdapter.Holder>() {
-    class Holder(view: View) : RecyclerView.ViewHolder(view) { // класс отвечает за отрисовку list_item
-        val binding = ListItemBinding.bind(view) // собираем все элементы из разметки list_item в один класс
-        fun bind(item: WeatherInfo) = with(binding) {
+    class Holder(view: View) : RecyclerView.ViewHolder(view){ // класс отвечает за отрисовку list_item
+        val binding = ListItemBinding.bind(view)  // собираем все элементы из разметки list_item в один объект
+
+        fun bind(item: WeatherInfo) = with(binding){
             tvListDate.text = item.time
             tvListCondition.text = item.condition
             tvListTemp.text = item.currentTemp
         }
+    }
+
+    class Comparator : DiffUtil.ItemCallback<WeatherInfo>(){  // обновление данных с сервера
+        override fun areItemsTheSame(oldItem: WeatherInfo, newItem: WeatherInfo): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: WeatherInfo, newItem: WeatherInfo): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder { // создаем Холдер
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        return Holder(view)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) { // отрисовка элементов
+        holder.bind(getItem(position))
     }
 }
--- app/src/main/java/com/practicum/myweatherapp/fragments/HoursFragment.kt
+++ app/src/main/java/com/practicum/myweatherapp/fragments/HoursFragment.kt
@@ -5,17 +5,48 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.practicum.myweatherapp.R
+import com.practicum.myweatherapp.adapters.WeatherAdapter
+import com.practicum.myweatherapp.adapters.WeatherInfo
+import com.practicum.myweatherapp.databinding.FragmentHoursBinding
 
 
 class HoursFragment : Fragment() {
 
+    private lateinit var binding : FragmentHoursBinding
+    private lateinit var adapter: WeatherAdapter
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hours, container, false)
+    ): View {
+        binding = FragmentHoursBinding.inflate(inflater, container, false)// Inflate the layout for this fragment
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRcView()
+    }
+
+    private fun initRcView() = with(binding) {
+        rcView.layoutManager = LinearLayoutManager(activity)
+        adapter = WeatherAdapter()
+        rcView.adapter = adapter
+        var list = listOf(
+            WeatherInfo("", "14:00",
+                "4°C","", "",
+                "Cloudly","",""),
+            WeatherInfo("", "15:00",
+                "6°C","", "",
+                "Cloudly","",""),
+            WeatherInfo("", "16:00",
+                "7°C","", "",
+                "Cloudly","","")  // для теста
+
+        )
+        adapter.submitList(list)
     }
 
     companion object {
